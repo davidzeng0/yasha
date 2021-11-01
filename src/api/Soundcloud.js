1dc25765aa90fb9be7cef303fe17399c6e0cae1b
davidzeng0 <40158558+davidzeng0@users.noreply.github.com>
2021-10-31 20:57:09 -0700

    update fetch to 3.0.0

--- src/api/Soundcloud.js
+++ src/api/Soundcloud.js
@@ -20,7 +20,7 @@ const soundcloudInterface = (new class{
 			try{
 				await this.data;
 			}catch(e){
-				Logger.error('LOAD', 'Could not load client id', e);
+				throw new SourceError.INTERNAL_ERROR('Could not load client id', e);
 
 				if(!e.fatal)
 					continue;
@@ -140,9 +140,33 @@ class SoundcloudTrack extends Track{
 
 		if(track.media && track.media.transcodings){
 			for(var stream of track.media.transcodings){
+				// if(stream.format.protocol == 'progressive'){
+				// 	const {resp, body} = await soundcloudInterface.abspathRequest(stream.url);
+
+				// 	result.streams = [{url: body.url}];
+
+				// 	break;
+				// }
+
 				if(stream.format.protocol == 'hls'){
 					var mime = /(audio)\/([a-zA-Z0-9]{3,4})(?:;(?:\+| )?codecs="(.*?)")?/.exec(stream.format.mime_type);
 					var data = await soundcloudInterface.abspathRequest(stream.url);
+
+					// var data = await APIUtil.get(url);
+
+					// const parser = new Parser();
+
+					// parser.push(data);
+					// parser.end();
+
+					// var dash = true;
+
+					// for(const segment of parser.segments)
+					// 	if(!/https:\/\/[^]+?\/media\/[0-9]+?\/[0-9]+?\/[^]/.exec(segment.uri)){
+					// 		dash = false;
+
+					// 		break;
+					// 	}
 
 					this.streams.push(
 						new SoundcloudStream(data.body.url)
@@ -201,6 +225,24 @@ class SoundcloudStreams extends TrackStreams{
 }
 
 var api = new (class{
+	// _fetchTracks(tracks, callback){
+	// 	var tr = [];
+
+	// 	for(var i = 0; i < tracks.length; i++)
+	// 		tr.push(tracks[i].id);
+	// 	request({method: 'GET', url: 'https://api-v2.soundcloud.com/tracks?ids=' + tr.join(encodeURIComponent(',')) + '&client_id=' + this.clientId}, (err, resp, body) => {
+	// 		if(err)
+	// 			callback(err, null);
+	// 		if(resp.statusCode < 200 || resp.statusCode >= 400)
+	// 			return callback(new Error('Error ' + resp.statusCode), null);
+	// 		var data = JSON.parse(body);
+
+	// 		for(var i = 0; i < data.length; i++)
+	// 			data[i] = this._makeResultFromTrack(data[i]);
+	// 		callback(null, data);
+	// 	});
+	// }
+
 	async resolve(url){
 		var {res, body} = await soundcloudInterface.makeRequest('resolve', {url: encodeURIComponent(url)});
 
@@ -218,6 +260,38 @@ var api = new (class{
 
 		throw new SourceError.INTERNAL_ERROR(null, new Error(body.kind + ' not supported'));
 	}
+
+	// get(url, callback){
+	// 	request({method: 'GET', url: 'https://api-v2.soundcloud.com/resolve?url=' + encodeURIComponent(url) + '&client_id=' + this.clientId}, (err, resp, body) => {
+	// 		if(err)
+	// 			return callback(err, null);
+	// 		if(resp.statusCode < 200 || resp.statusCode >= 400)
+	// 			return callback(new Error('Error ' + resp.statusCode), null);
+	// 		var data = JSON.parse(body);
+
+	// 		if(data.kind == 'track')
+	// 			callback(null, {track: this._makeResultFromTrack(data)});
+	// 		else if(data.tracks){
+	// 			var results = [];
+	// 			var rf = [];
+
+	// 			for(var i = 0; i < data.tracks.length; i++)
+	// 				if(data.tracks[i].user && data.tracks[i].id)
+	// 					results.push(this._makeResultFromTrack(data.tracks[i]));
+	// 				else
+	// 					rf.push(data.tracks[i]);
+	// 			if(rf.length)
+	// 				this._fetchTracks(rf, (err, data) => {
+	// 					if(err)
+	// 						return callback(err, null);
+	// 					callback(null, {playlist: results.concat(data)});
+	// 				})
+	// 			else
+	// 				callback(null, {playlist: results});
+	// 		}else
+	// 			callback(new Error('Unsupported soundcloud type ' + data.kind), null);
+	// 	});
+	// }
 
 	async search(query, offset, limit = 20){
 		var {res, body} = await soundcloudInterface.makeRequest('search/tracks', {q: encodeURIComponent(query), limit, offset});
--- src/fetch.js
+++ src/fetch.js
@@ -1,10 +1,10 @@
-const fetch = require('node-fetch');
+const fetch = import('node-fetch');
 
 const httpsAgent = new (require('https').Agent)({keepAlive: true});
 const httpAgent = new (require('http').Agent)({keepAlive: true});
 
-module.exports = function(url, opts = {}){
+module.exports = async function(url, opts = {}){
 	opts.agent = new URL(url).protocol == 'https:' ? httpsAgent : httpAgent;
 
-	return fetch(url, opts);
+	return (await fetch).default(url, opts);
 };
